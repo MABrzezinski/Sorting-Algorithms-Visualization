510570f340b7bca68d7058dd749194703226ceb1
MABrzezinski <czesc@brzezinskimichal.pl>
2021-11-23 21:08:04 +0100

    implement heap sort

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-//--------MAIN--------//
+//--MAIN----------------------------------------------------------------------------//
 //Below you find the code responsible for maintenance of columns
 
 // Array of column-objects - the columns array
@@ -34,12 +34,36 @@ function removeAllChildNodesTable() {
 }
 
 // Function for rendering columns array
-function Render() {
+function render() {
   removeAllChildNodesTable();
   for (i = 0; i < columns.length; i++) {
     let columns_parent = document.getElementById("columns_parent");
     let colToAdd = document.createElement("td");
-    colToAdd.setAttribute("id", columns[i].id);
+    // colToAdd.setAttribute("id", columns[i].id);
+    colToAdd.style.setProperty("--size", columns[i].value);
+    colToAdd.style.setProperty("--color", columns[i].color);
+    colToAdd.innerHTML = columns[i].value;
+    columns_parent.appendChild(colToAdd);
+  }
+}
+
+// Does the same what render() does but first it renders array with sorted elements for heap sort and then remaining elements
+// from columns array.
+function renderForHeap() {
+  removeAllChildNodesTable();
+  for (i = 0; i < heapArray.length; i++) {
+    let columns_parent = document.getElementById("columns_parent");
+    let colToAdd = document.createElement("td");
+    // colToAdd.setAttribute("id", heapArray[i].id);
+    colToAdd.style.setProperty("--size", heapArray[i].value);
+    colToAdd.style.setProperty("--color", heapArray[i].color);
+    colToAdd.innerHTML = heapArray[i].value;
+    columns_parent.appendChild(colToAdd);
+  }
+  for (i = 0; i < columns.length; i++) {
+    let columns_parent = document.getElementById("columns_parent");
+    let colToAdd = document.createElement("td");
+    // colToAdd.setAttribute("id", columns[i].id);
     colToAdd.style.setProperty("--size", columns[i].value);
     colToAdd.style.setProperty("--color", columns[i].color);
     colToAdd.innerHTML = columns[i].value;
@@ -50,7 +74,9 @@ function Render() {
 // Function that spawn new column
 function Spawn() {
   columns.push(new Column(RandColor(), RandValue()));
-  Render();
+  render();
+  //heapIndex is defined in Heap Sort section. It is being updated after every spawn, because we want to heapify from the last element of the columns array.
+  heapIndex = columns.length - 1;
 }
 
 // Function for randomizing values of spawned columns
@@ -58,10 +84,11 @@ function Randomize() {
   for (i = 0; i < columns.length; i++) {
     columns[i].value = RandValue();
   }
-  Render();
+  render();
   // In case of situation where user calls Randomize function before algorithm finished sorting
   bubbleIndex = 0;
   selectIndex = 0;
+  heapIndex = columns.length - 1;
 }
 
 // Function that deletes all paragraphs from from under the table
@@ -82,13 +109,13 @@ function renderIndicator(message) {
   indicators_parent.appendChild(msgToAdd);
 }
 
-//--------SORTING--------//
+//--SORTING----------------------------------------------------------------------------//
 //Below you find the code responsible for sorting algorithms
 
 //Variable shared by all sorting algorithms functions, it holds interval id for setInterval methods
 let interval;
 
-//--Insertion Sort--//
+//---Insertion Sort---//
 
 //This function is one "step" of the insertion sort
 function insertionSortStep() {
@@ -116,18 +143,18 @@ function insertionSort() {
   renderIndicator("Sorting in progress...");
   interval = setInterval(function () {
     insertionSortStep();
-    Render();
+    render();
   }, 500);
 }
 
-//--Bubble Sort--//
+//---Bubble Sort---//
 
 //Value of this variable tells which element of the columns array is being tested in current step.
 //bubbleIndex resets after iterating through all elements of the columns array.
 let bubbleIndex = 0;
 
 //Function bubbleSortStep "knows" that array is sorted when number of noSwaps matches columns.length - 1.
-//NoSwaps incrementation occurs when... tested element doesn't have to be swapped with next element.
+//NoSwaps incrementation occurs when tested element doesn't have to be swapped with next element.
 let noSwaps = 0;
 
 //This function is one "step" of the bubble sort
@@ -138,7 +165,7 @@ function bubbleSortStep() {
   if (columns[bubbleIndex].value > columns[bubbleIndex + 1].value) {
     columns[bubbleIndex] = nextElement;
     columns[bubbleIndex + 1] = currentElement;
-    Render();
+    render();
   } else {
     noSwaps++;
   }
@@ -164,11 +191,11 @@ function bubbleSort() {
   renderIndicator("Sorting in progress...");
   interval = setInterval(function () {
     bubbleSortStep();
-    Render();
+    render();
   }, 500);
 }
 
-//--Selection Sort--//
+//---Selection Sort---//
 
 //Value of this variable tells which element of the columns array is being tested in current step.
 let selectIndex = 0;
@@ -201,6 +228,101 @@ function selectionSort() {
   renderIndicator("Sorting in progress...");
   interval = setInterval(function () {
     selectionSortStep();
-    Render();
+    render();
+  }, 500);
+}
+
+//---Heap Sort---//
+
+// Heap property
+// -------------
+// leftChildIndex = (2 * parentIndex) + 1
+// so...
+// parentIndex = (leftChildIndex - 1) / 2
+//
+// rightChildIndex = (2 * parentIndex) + 2
+// parentIndex = (rightChildIndex - 2) / 2
+
+// Variable holds index of rightmost unheapified node.
+let heapIndex = columns.length - 1;
+
+//Function minHeapSortStep "knows" that array is heapified when number of noSwaps matches number of parent nodes.
+//NoSwaps incrementation occurs when tested parent-child trio or duo doesn't need to be swapped between each other.
+let heapNoSwaps = 0;
+
+// Array of sorted elements.
+let heapArray = [];
+
+//This function 
+function minHeapSortStep() {
+
+  //If heapIndex points to the root of the tree then it starts from the end.
+  if (heapIndex === 0) {
+    heapIndex = columns.length - 1;
+    heapNoSwaps = 0;
+  }
+
+  //Checks if the rightmost unheapified child-node is a right child (which means it has a sibling) or a left child (means it has not).
+  //Since root element has index 0, if checked node has index represented by even number - it means it is the node is right child.
+  if (heapIndex % 2 === 0) { //Two child nodes.
+    
+    //Finds child node with lower value.
+    let lowerOfTwo = columns[heapIndex].value < columns[heapIndex - 1].value ? heapIndex : heapIndex - 1;
+
+    //If child node with lower value has lower value than parent, then they swap.
+    if (columns[lowerOfTwo].value < columns[(heapIndex - 2) / 2].value) { //explained in comment about heap property.
+      [columns[lowerOfTwo], columns[(heapIndex - 2) / 2]] = [columns[(heapIndex - 2) / 2], columns[lowerOfTwo]];
+      // Moves to another rightmost unheapified parent - children trio.
+      heapIndex = heapIndex - 2;
+
+    } else { // Parent has lower value than children. There's no swap. 
+      heapNoSwaps++;
+      // Moves to another parent with two child nodes.
+      heapIndex = heapIndex - 2;
+    }
+
+  } else { //One child node.
+
+    //If child node has lower value than parent, then they swap.
+    if (columns[heapIndex].value < columns[(heapIndex - 1) / 2].value) {
+      [columns[heapIndex], columns[(heapIndex - 1) / 2]] = [columns[(heapIndex - 1) / 2], columns[heapIndex]];
+
+      // Moves to another rightmost unheapified parent - children trio. 
+      heapIndex--;
+
+    } else { // Parent has lower value than children. There's no swap. 
+      heapNoSwaps++;
+      
+      // Moves to another rightmost unheapified parent - children trio.
+      heapIndex--;
+    }
+  }
+
+  //If number of heapNoSwaps equals number of possible swaps in given tree, it means array is heapified.
+  //Root element goes to array with sorted elements. Last element of columns array take the root place.
+  if (heapNoSwaps === Math.ceil((columns.length - 1) / 2)) {
+    console.log("heapified");
+    heapArray.push(columns[0]);
+    columns[0] = columns[columns.length - 1];
+    columns.splice(columns.length - 1, 1);
+  }
+
+  renderForHeap();
+
+  if (columns.length === 1) {
+    heapArray.push(columns[0]);
+    columns = heapArray;
+    heapArray = [];
+    clearInterval(interval);
+    console.log("Columns are sorted");
+    renderIndicator("Sorting is finished. Randomize and sort again :)");
+  }
+}
+
+//This function calls one step of the heap sort every given interval
+function heapSort() {
+  renderIndicator("Sorting in progress...");
+  interval = setInterval(function () {
+    minHeapSortStep();
   }, 500);
 }
